c9b28f5d789c900344a6472137c383d4eb4ec0c2
Kevin Tan <kevmtan@login2.(none)>
2017-06-29 19:46:30 -0700

    Initial Hoffman2, both mpragembw & 1struct

---
+++ spm12_dartel_mprageMBW/run_dartel_mprageMBW.m
@@ -0,0 +1,174 @@
+function [status, errorMsg, allfuncs, allt1, allrc1, allrc2, allu_rc1] =...
+    run_dartel_mprageMBW(subNam, owd, codeDir, output, runID, funcID, mbwdirID,...
+    mpragedirID, fourDnii, execPreDartel)
+
+%% Parameters
+funcFormat=2;       % format of your raw functional images (1=img/hdr, 2=4D nii)
+
+%% Make Matlabbatch
+spm('defaults','fmri'); spm_jobman('initcfg');
+
+status = NaN;
+errorMsg = '';
+allfuncs = [];
+allt1 = [];
+allrc1 = [];
+allrc2 = [];
+allu_rc1 = [];
+
+try
+    cd(owd)
+    swd = sprintf('%s/%s',owd,subNam);
+    fprintf('Subject directory is %s\n',swd)
+    cd(swd)
+    cd raw
+    base_dir = pwd;
+    
+    % Find run directories
+    d=dir(runID);
+    run_names = {d.name};
+    numruns=length(run_names);
+    fprintf('Found %d runs\n',numruns)
+    
+    % Find functional images for run(s)
+    %-----------------------------------------------------------------%
+    load_dir = {};
+    raw_func_filenames = {};
+    allfiles_orig = {};
+    allfiles_norm = {};
+    for i = 1:numruns
+        load_dir{i} = fullfile(base_dir,run_names{i});
+        if funcFormat==1
+            tmpString=sprintf('^%s.*\\.img$',funcID);
+            [raw_func_filenames{i},dirs] = spm_select('List',load_dir{i},tmpString, inf);
+            filenames_orig{i}=cellstr(strcat(load_dir{i},filesep,raw_func_filenames{i}));
+            filenames_norm{i}=cellstr(strcat(load_dir{i},filesep,'w',raw_func_filenames{i}));
+            allfiles_orig = [allfiles_orig; filenames_orig{i}];
+        else
+            tmpString=sprintf('^%s.*\\.nii$',funcID);
+            [raw_func_filenames{i},dirs] = spm_select('ExtFPList',load_dir{i},tmpString, inf);
+            filenames_orig{i}=cellstr(strcat(raw_func_filenames{i}));
+            filenames_norm{i}=cellstr(strcat('w',raw_func_filenames{i}));
+            allfiles_orig = [allfiles_orig; filenames_orig{i}];
+        end
+    end
+    if funcFormat==1
+        mean_func=cellstr(strcat(load_dir{1},filesep,'mean',raw_func_filenames{1}(1,:)));
+    else
+        [path name ext] = fileparts(allfiles_orig{1});
+        mean_func=cellstr(strcat(path,filesep,'mean',name,'.nii'));
+    end
+    load_dir = fullfile(base_dir,run_names{i});
+    
+    % Find the anatomicals
+    % -------------------------------------------------
+    
+    % find the mbw folder
+    d=dir(mbwdirID);
+    mbwdir = [base_dir filesep d(1).name];
+    
+    % find the mprage folder
+    d=dir(mpragedirID);
+    mprdir = [base_dir filesep d(1).name];
+    
+    % get the images
+    cd(mbwdir); d = dir([mbwdirID '.nii']);
+    mbw_name = d.name; clear d
+    cd(mprdir); d = dir([mpragedirID '.nii']);
+    mprage_name = d.name; clear d
+    mbw = [mbwdir filesep mbw_name];
+    t1vol = [mprdir filesep mprage_name];
+    fprintf('MBW is: %s\n',mbw)
+    fprintf('MPRAGE is: %s\n\n',t1vol)
+    [firstpart,lastpart] = strread(t1vol,'%s %s','delimiter','.');
+    
+    % for DARTEL
+    if fourDnii == 1
+        allfuncs = filenames_orig;
+    else
+        allfuncs = allfiles_orig;
+    end
+    allt1 = t1vol;
+    allrc1 = [mprdir filesep 'rc1' mprage_name(1:end-4) '.nii'];
+    allrc2 = [mprdir filesep 'rc2' mprage_name(1:end-4) '.nii'];
+    allu_rc1 = [mprdir filesep 'u_rc1' mprage_name(1:end-4) '_Template.nii'];
+    
+    % =====================================
+    % Begin building MATLABBATCH
+    % =====================================
+    
+    matlabbatch{1}.cfg_basicio.cfg_cd.dir = cellstr(strcat(swd,filesep,'notes'));
+    
+    % Realignment of functionals
+    % -------------------------------------------------
+    for i = 1:numruns
+        matlabbatch{2}.spm.spatial.realign.estwrite.data{i} = filenames_orig{i};
+    end
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;         % higher quality
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.sep = 4;               % default is 4
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;              % default
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.rtm = 1;               % changed from 0 (=realign to first) to 1 (realign to mean) for
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.interp = 4;            % default
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];        % default
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.weight = {};           % don't weight
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.which  = [0 1];        % create mean image only when reslicing
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.interp = 4;            % default
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.wrap   = [0 0 0];      % no wrap (default)
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.mask   = 1;            % enable masking (default)
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+    
+    % Co-register MBW to MEAN FUNCTIONAL
+    % -------------------------------------------------
+    matlabbatch{3}.spm.spatial.coreg.estimate.ref = mean_func;
+    matlabbatch{3}.spm.spatial.coreg.estimate.source = cellstr(mbw);
+    matlabbatch{3}.spm.spatial.coreg.estimate.other{1} = '';
+    matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+    matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+    matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+    matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
+    
+    % Co-register MPRAGE to MBW
+    % -------------------------------------------------
+    matlabbatch{4}.spm.spatial.coreg.estimate.ref = cellstr(mbw);
+    matlabbatch{4}.spm.spatial.coreg.estimate.source = cellstr(t1vol);
+    matlabbatch{4}.spm.spatial.coreg.estimate.other{1} = '';
+    matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+    matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+    matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+    matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
+    
+catch
+    status = 0;
+    errorMsg = 'Error making matlabbatch';
+    disp([errorMsg ' for ' subNam]);
+    cd(codeDir);
+    return
+end
+
+%% Save matlabbatch
+try
+    time_stamp = datestr(now, 'yyyymmdd_HHMM');
+    filename = [output '/preDARTEL_MBWmprage_' subNam '_' time_stamp];
+    save(filename, 'matlabbatch');
+catch
+    status = 0;
+    errorMsg = 'Error saving matlabbatch';
+    disp([errorMsg ' for ' subNam]);
+    cd(codeDir);
+    return
+end
+%% Run Matlabbatch
+try
+    if execPreDartel == 1
+        spm_jobman('run',matlabbatch);
+    end
+    status = 1;
+    cd(codeDir);
+catch
+    status = 0;
+    errorMsg = 'Error running matlabbatch';
+    disp([errorMsg ' for ' subNam]);
+    cd(codeDir);
+    return
+end
+end
---
+++ spm12_dartel_mprageMBW/wrapper_dartel_mprageMBW.m
@@ -0,0 +1,240 @@
+%% SPM12 Dartel using both MPRAGE & MBW
+% Created by Kevin Tan on Jun 29, 2017 (some code adopted from Bob Spunt)
+
+% DESCRIPTION:
+% 1) Functionals realigned to mean functional orientation
+% 2) MBW co-registered with mean functional (saved in nifti header)
+% 3) MPRAGE co-registered with MBW (saved in nifti header)
+% 4) Segment MPRAGE
+% 5) Create DARTEL templates
+% 6) Normalize functionals to MNI space via DARTEL
+% 7) Normalize MPRAGE to MNI space via DARTEL
+
+% INSTRUCTIONS:
+% Edit parameters at the top of the wrapper then run the wrapper. The wrapper will
+% call "run_dartel_mprageMBW" so make sure that is in the same directory. This
+% will save a "runStatus" struct that will contain each subjects'
+% pre-Dartel status. It will also save the pre-Dartel workspace that will
+% be used for Dartel. 
+
+
+%% User-editable Parameters
+
+% Path/directory/name information
+owd = '/u/project/sanscn/kevmtan/scripts/SPM12_DARTEL/spm12_dartel_mprageMBW/PTSD';  % base study directory
+codeDir = '/u/project/sanscn/kevmtan/scripts/SPM12_DARTEL/spm12_dartel_mprageMBW'; % where code lives
+output = '/u/project/sanscn/kevmtan/scripts/SPM12_DARTEL/spm12_dartel_mprageMBW/PTSDbatch'; % dir in which to save scripts
+subID = 'VET*'; % pattern for finding subject folders (use wildcards)
+runID = 'BOLD_*'; % pattern for finding functional run folders (use wildcards)
+funcID ='BOLD_'; % first character(s) in your functional images? (do NOT use wildcards)
+mbwdirID = 'Matched_Bandwidth_HiRes*'; % pattern for finding matched-bandwidth folder (use wildcards)
+mpragedirID = 'SAG_MPRAGE*'; % pattern for finding mprage folder (use wildcards)
+
+% Subjects to do/skip
+subNam = {}; % do which subjects? (leave empty to do all)
+skipSub = {};
+
+% 4d or 3d functional .nii?
+fourDnii = 0; % 1=4d, 0=3d
+
+% Path of TPM tissues in your SPM directory
+tpmPath = '/u/project/CCN/apps/spm12/tpm';
+
+% Customizable preprocessing parameters
+vox_size=3;    % voxel size at which to re-sample functionals (isotropic)
+smooth_FWHM=8; % smoothing kernel (isotropic)
+
+% Execute (1) or just make matlabbatches (0)
+execPreDartel = 0;
+execDartel = 0;
+
+%% Setup subjects
+
+% Find subject directories
+if isempty(subNam)
+    d = dir([owd '/' subID]);
+    for ii = 1:length(d)
+        subNam{ii} = d(ii).name;
+        fprintf('Adding %s\n', subNam{ii})
+    end
+end
+numSubs = length(subNam);
+cd(codeDir);
+
+% Prepare status struct
+runStatus = struct('subNam',[],'status',[],'error',[]);
+runStatus(numSubs).subNam = [];
+runStatus(numSubs).status = [];
+runStatus(numSubs).error = [];
+
+%% Run Pre-dartel (explicitly parallelized per subject)
+try
+    mkdir(output);
+catch
+end
+
+pool = parpool('local', numSubs);
+parfor i = 1:numSubs
+    % Pre-allocate subject in runStatus struct
+    runStatus(i).subNam = subNam{i};
+    
+    % Cross-check subject with run/skip list
+    if ismember(subNam{i}, skipSub)
+        runStatus(i).status = 0;
+        runStatus(i).error = 'Subject in exclusion list';
+        disp(['Skipping subject ' subNam{i} ', is in exclusion list']);
+        continue
+    else % Run subject
+        disp(['Running subject ' subNam{i}]);
+        [runStatus(i).status, runStatus(i).error, a_allfuncs{i}, allt1{i},...
+            allrc1{i}, allrc2{i}, allu_rc1{i}] = run_dartel_mprageMBW(subNam{i},...
+            owd, codeDir, output, runID, funcID, mbwdirID, mpragedirID, fourDnii, execPreDartel);
+        if runStatus(i).status == 1
+            disp(['subject ' subNam{i} ' successful']);
+        else
+            runStatus(i).status = 0;
+            disp([runStatus(i).error ' for ' subNam{i}]);
+        end
+    end
+end
+delete(pool);
+
+% Get rid of image frame specification for func images (SPM idiosyncrasy)
+if fourDnii == 1
+    for ia = 1:length(a_allfuncs)
+        for ib = 1:length(a_allfuncs{ia})
+            chars = length(a_allfuncs{ia}{ib}{1})-2;
+            allfuncs{ia}{ib,1} = a_allfuncs{ia}{ib}{1}(1:chars);
+        end
+    end
+else
+    for ia = 1:length(a_allfuncs)
+        for ib = 1:length(a_allfuncs{ia})
+            chars = length(a_allfuncs{ia}{ib})-2;
+            allfuncs{ia}{ib,1} = a_allfuncs{ia}{ib}(1:chars);
+        end
+    end
+end
+
+% Save stuff
+date = datestr(now,'yyyymmdd_HHMM');
+filename = [output '/runStatus_' date '.mat'];
+save(filename,'runStatus');
+filename = [output '/forDartel_workspace_' date '.mat']; % Use this to re-do "run dartel" if it fails
+save(filename);
+
+%% Create DARTEL matlabbatch
+
+spm('defaults','fmri'); spm_jobman('initcfg');
+
+% Subjects with no errors in pre-dartel
+inds = find([runStatus.status]);
+
+% Create Segmentation matlabbatch (implicitly parallelized in SPM)
+for s = 1:length(inds)
+    matlabbatch{1}.spm.spatial.preproc.channel.vols{s,1} = allt1{inds(s)};
+end
+matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.0001;
+matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.preproc.channel.write = [0 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = cellstr([tpmPath '/TPM.nii,1']);
+matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = cellstr([tpmPath '/TPM.nii,2']);
+matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = cellstr([tpmPath '/TPM.nii,3']);
+matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = cellstr([tpmPath '/TPM.nii,4']);
+matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
+matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = cellstr([tpmPath '/TPM.nii,5']);
+matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
+matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = cellstr([tpmPath '/TPM.nii,6']);
+matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.warp.mrf = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.cleanup = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.preproc.warp.affreg = 'mni';
+matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
+matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
+matlabbatch{1}.spm.spatial.preproc.warp.write = [0 1];
+
+% DARTEL: create templates
+for s = 1:length(inds)
+    matlabbatch{2}.spm.tools.dartel.warp.images{1,1}{s,1} = allrc1{inds(s)};
+    matlabbatch{2}.spm.tools.dartel.warp.images{1,2}{s,1} = allrc2{inds(s)};
+end
+matlabbatch{2}.spm.tools.dartel.warp.settings.template = 'Template';
+matlabbatch{2}.spm.tools.dartel.warp.settings.rform = 0;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(1).its = 3;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(1).rparam = [4 2 1e-06];
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(1).K = 0;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(1).slam = 16;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(2).its = 3;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(2).rparam = [2 1 1e-06];
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(2).K = 0;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(2).slam = 8;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(3).its = 3;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(3).rparam = [1 0.5 1e-06];
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(3).K = 1;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(3).slam = 4;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(4).its = 3;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(4).rparam = [0.5 0.25 1e-06];
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(4).K = 2;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(4).slam = 2;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(5).its = 3;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(5).rparam = [0.25 0.125 1e-06];
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(5).K = 4;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(5).slam = 1;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(6).its = 3;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(6).rparam = [0.25 0.125 1e-06];
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(6).K = 6;
+matlabbatch{2}.spm.tools.dartel.warp.settings.param(6).slam = 0.5;
+matlabbatch{2}.spm.tools.dartel.warp.settings.optim.lmreg = 0.01;
+matlabbatch{2}.spm.tools.dartel.warp.settings.optim.cyc = 3;
+matlabbatch{2}.spm.tools.dartel.warp.settings.optim.its = 3;
+
+% DARTEL: normalize functional images to MNI
+matlabbatch{3}.spm.tools.dartel.mni_norm.template(1) = cfg_dep('Run Dartel (create Templates): Template (Iteration 6)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','template', '()',{7}));
+for s = 1:length(inds)
+    matlabbatch{3}.spm.tools.dartel.mni_norm.data.subj(s).flowfield{1} = allu_rc1{inds(s)};
+    matlabbatch{3}.spm.tools.dartel.mni_norm.data.subj(s).images = allfuncs{inds(s)};
+end                                             
+matlabbatch{3}.spm.tools.dartel.mni_norm.vox = [vox_size vox_size vox_size];
+matlabbatch{3}.spm.tools.dartel.mni_norm.bb = [-78 -112 -50; 78 76 85];
+matlabbatch{3}.spm.tools.dartel.mni_norm.preserve = 0;
+matlabbatch{3}.spm.tools.dartel.mni_norm.fwhm = [smooth_FWHM smooth_FWHM smooth_FWHM];
+
+% DARTEL: normalize MPRAGE to MNI
+matlabbatch{4}.spm.tools.dartel.mni_norm.template(1) = cfg_dep('Run Dartel (create Templates): Template (Iteration 6)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','template', '()',{7}));
+for s = 1:length(inds)
+    matlabbatch{4}.spm.tools.dartel.mni_norm.data.subj(s).flowfield{1} = allu_rc1{inds(s)};
+    matlabbatch{4}.spm.tools.dartel.mni_norm.data.subj(s).images{1} = allt1{inds(s)};
+end                                            
+matlabbatch{4}.spm.tools.dartel.mni_norm.vox = [1 1 1];
+matlabbatch{4}.spm.tools.dartel.mni_norm.bb = [-78 -112 -50; 78 76 85];
+matlabbatch{4}.spm.tools.dartel.mni_norm.preserve = 0;
+matlabbatch{4}.spm.tools.dartel.mni_norm.fwhm = [0 0 0];
+
+%% Save & run DARTEL matlabbatch
+
+% save matlabbatch struct in output folder
+time_stamp = datestr(now,'yyyymmdd_HHMM');
+filename = [output '/DARTEL_MBWmprage_' time_stamp];
+save(filename,'matlabbatch');                         
+
+% Execute matlabbatch
+if execDartel == 1
+    spm_jobman('run',matlabbatch);
+    disp('DARTEL COMPLETED');
+end
